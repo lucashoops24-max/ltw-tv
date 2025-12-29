e792f232593221edb23abf9d59ed056fa40a7333
lucashoops24-max <lucashoops24@gmail.com>
2025-12-29 09:37:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+// USER (demo login)
+if (!localStorage.getItem("user")) {
+  localStorage.setItem("user", JSON.stringify({
+    name: "GuestUser",
+    avatar: "https://i.pravatar.cc/100"
+  }));
+}
+
+const user = JSON.parse(localStorage.getItem("user"));
+
+// VIDEOS DATABASE
+const videos = {
+  "vid1": {
+    title: "Love of Christ",
+    category: "sermons",
+    likes: 0,
+    dislikes: 0,
+    comments: []
+  }
+};
+
+localStorage.setItem("videos", JSON.stringify(
+  JSON.parse(localStorage.getItem("videos")) || videos
+));
 
